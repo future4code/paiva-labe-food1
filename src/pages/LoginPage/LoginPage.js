c237538445ad995f0258df44dac42b513f75830e
Vitor Suzuki <vitor.szk@outlook.com>
2021-06-29 14:04:12 -0300

    commit de correção de PR

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { ScreenContainer, LogoImage, H1, InputsContainer, Form, SignUpText } from './styled';
+import logo from '../../assets/logo.svg';
+import useForm from '../../hooks/useForm';
 
 const LoginPage = () => {
+  const [form, onChange, cleanFields] = useForm({ email: "", passssword: "" });
+
+  const onSubmit = () => {
+
+  }
+
   return (
-    <div>
-      <h1>Tela de Login</h1>
-    </div>
+    <ScreenContainer>
+      <LogoImage src={logo} />
+      <H1>Entrar</H1>
+      <InputsContainer>
+        <Form onSubmit={onSubmit}>
+          <input
+            type={"email"}
+            name={"email"}
+            placeholder={"email@email.com"}
+            value={form.email}
+            onChange={onChange}
+            required
+          >
+          </input>
+
+          <input
+            type={"password"}
+            name={"passsword"}
+            placeholder={"Mínimo 6 caracteres"}
+            value={form.password}
+            onChange={onChange}
+            required
+          >
+          </input>
+          <button>Entrar</button>
+          <SignUpText>Não possui Cadastro? Clique aqui.</SignUpText>
+        </Form>
+      </InputsContainer>
+    </ScreenContainer>
   );
 }
 
